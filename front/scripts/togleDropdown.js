e50f109c42a32701837feb37b6e60ca06c30f386
Dandan <97252704+Daniel-Centeno@users.noreply.github.com>
2024-01-31 22:02:38 -0300

    update do front

---
+++ front/scripts/togleDropdown.js
@@ -0,0 +1,23 @@
+function toggleDropdown() {
+     var dropdown = document.querySelector('.dropdown');
+     dropdown.classList.toggle('active');
+ }
+
+// function showSideBar() {
+//     document.getElementById('tbutton').style.marginLeft = '40%';
+// }
+
+// Side bar button logic
+let toggled;
+$(document).ready(function() {
+    $('#tbutton').on('click', function() {
+        if (toggled) {
+            $('#sidebar').css('margin-left', '-20vw');
+            toggled = false;
+        } else {
+            $('#sidebar').css('margin-left', '0vw');
+            toggled = true;
+        }
+        
+    });
+});
