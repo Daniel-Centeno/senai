d4ce006f70407bc21284d3b89017858ca7efbb9e
Dandan <97252704+Daniel-Centeno@users.noreply.github.com>
2024-02-01 22:06:18 -0300

    mais viadagens

--- front/scripts/togleDropdown.js
+++ front/scripts/togleDropdown.js
@@ -13,11 +13,29 @@ $(document).ready(function() {
     $('#tbutton').on('click', function() {
         if (toggled) {
             $('#sidebar').css('margin-left', '-20vw');
+            $('.conteudo').css('bottom', '29%');
             toggled = false;
         } else {
             $('#sidebar').css('margin-left', '0vw');
+            $('.conteudo').css('bottom', '13%');
             toggled = true;
         }
         
     });
 });
+
+function Pop() {
+    alert("clicou");
+}
+
+function Animated(){
+    var element = document.getElementById('sideBar');
+
+    // Add the 'animate' class to trigger the animation
+    element.classList.add('animation');
+
+    // Remove the 'animate' class after the animation is complete
+    element.addEventListener('animationend', function() {
+      element.classList.remove('animation');
+    });
+}
